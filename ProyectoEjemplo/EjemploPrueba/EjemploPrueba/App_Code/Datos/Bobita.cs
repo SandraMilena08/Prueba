faf3751b897a477e574d775fdbfb031a6e9b3a70
YEISON CIFUENTES <yeisoncifuentes2@gmail.com>
2020-03-04 16:24:24 -0500

    atributo

--- ProyectoEjemplo/EjemploPrueba/EjemploPrueba/App_Code/Datos/Bobita.cs
+++ ProyectoEjemplo/EjemploPrueba/EjemploPrueba/App_Code/Datos/Bobita.cs
@@ -11,6 +11,7 @@ public class Bobita
     private int edad;
     private int matitas;
     private int tontita;
+    private String hola;
         //
         // TODO: Agregar aquí la lógica del constructor
         //
